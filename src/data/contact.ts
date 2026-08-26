e351de5933037bd338b58f47f3ab1a2502541cdb
aphan4 <aphan4@uw.edu>
2026-08-26 11:15:12 -0700

    Update resume link

--- src/data/contact.ts
+++ src/data/contact.ts
@@ -3,5 +3,5 @@ export const CONTACT = {
     linkedinLabel: "linkedin.com/in/audrey-phan-1446bb289",
     linkedinHref: "https://www.linkedin.com/in/audrey-phan-1446bb289",
     resumeHref:
-        "https://drive.google.com/file/d/1ICzPCOPqdBpiBgrqt7kr7_kFGP592cim/view?usp=sharing",
+        "https://drive.google.com/file/d/1lVJ6sChRnCin3qplJcqqKuHt_0Wg4GXR/view?usp=sharing",
 }
